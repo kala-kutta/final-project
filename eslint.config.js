4a213b6818a89e9f2de4f126fe4c9fc1d31e6032
ohshitnotgood <praanto@icloud.com>
2025-11-04 21:39:05 +0100

    #4: moved lint step before build step and ignored dist and node modules from lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescri
 import pluginVue from "eslint-plugin-vue";
 
 export default defineConfigWithVueTs(
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
   pluginVue.configs["flat/recommended"],
   vueTsConfigs.recommended,
   vueTsConfigs.recommendedTypeChecked,
